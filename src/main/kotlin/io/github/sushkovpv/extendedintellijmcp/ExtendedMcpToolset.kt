ac8325f4774a59904297340a2eb88d5d4282252c
sushkov-pv <sushkov.pavel.98@gmail.com>
2025-12-22 22:58:40 +0300

    Refine regex search description in `@McpDescription` for clarity

--- src/main/kotlin/io/github/sushkovpv/extendedintellijmcp/ExtendedMcpToolset.kt
+++ src/main/kotlin/io/github/sushkovpv/extendedintellijmcp/ExtendedMcpToolset.kt
@@ -42,8 +42,7 @@ class ExtendedMcpToolset : McpToolset {
     @McpTool
     @McpDescription(
         """
-        |Searches with a regex pattern within project dependencies (libraries) using IntelliJ's search engine.
-        |Prefer this tool over reading files with command-line tools because it's much faster.
+        |Searches with a regex pattern within the files of project dependencies (libraries) using IntelliJ's search engine.
         |
         |The result occurrences are surrounded with || characters, e.g. `some text ||substring|| text`
     """
